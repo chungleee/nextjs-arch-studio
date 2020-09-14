a1cb7ba0fef2980c7315dd703e9a9f8ef7472fe8
Leon <leon.karho.chung@gmail.com>
2020-09-14 14:25:38 -0400

    components folder

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/assets/logo.svg" alt="Arch Logo" />
+    </header>
+  );
+};
+
+export default Header;
