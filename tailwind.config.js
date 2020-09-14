33547d51e2bc0afd896046baa903062ddaf81959
Leon <leon.karho.chung@gmail.com>
2020-09-14 15:23:20 -0400

    import google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Spartan", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "very-dark-blue": "#1B1D23",
+        "dark-grey": "#60636D",
+        "medium-grey": "#7D828F",
+        "light-grey": "#C8CCD8",
+        "very-light-grey": "#EEEFF4",
+        "error-red": "#DF5656",
+      },
+    },
   },
   variants: {},
   plugins: [],
