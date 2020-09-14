92e098140f48a4e745a2e011cd6abfe614ebaf99
Leon <leon.karho.chung@gmail.com>
2020-09-14 16:47:35 -0400

    header component with logo and hamburger

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Hamburger from "./icons/Hamburger";
+import Logo from "./icons/Logo";
 
 const Header = () => {
   return (
-    <header>
-      <img src="/assets/logo.svg" alt="Arch Logo" />
+    <header className="border flex justify-between items-center px-8 py-8">
+      <img className="h-8" src="/assets/logo.svg" alt="Arch Logo" />
+      <span>
+        <Hamburger />
+      </span>
     </header>
   );
 };
-
 export default Header;
