7c2ac96676847eff7aab29ef4771059f094661bb
Leon <leon.karho.chung@gmail.com>
2020-09-16 15:59:46 -0400

    styling + page content

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,43 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <section className="bg-img-paramour bg-no-repeat flex items-center h-screen px-8 text-white">
+      <section className="bg-img-paramour  bg-no-repeat flex items-center h-screen px-8 text-white">
         <div>
-          <h1 className="capitalize font-bold leading-none mb-4 text-5xl">
+          <h1 className="capitalize font-bold leading-none mb-4 text-4xl">
             project paramour
           </h1>
           <p className="leading-6 mb-12 text-left text-sm">
             Project made for an art museum near Southwest London. Project
             Paramour is a statement of bold, modern architecture.
           </p>
-          <Button>See Our Portfolio</Button>
+          <Button className="font-bold text-sm">See Our Portfolio</Button>
+        </div>
+      </section>
+      <section className="px-8">
+        <div className="py-12">
+          <hr className="border-light-grey w-16 mb-12" />
+          <h1 className="font-bold mb-10 leading-none text-very-dark-blue text-4xl">
+            Welcome to <br /> Arch Studio
+          </h1>
+          <div className="leading-6 text-sm text-dark-grey">
+            <p className="mb-6">
+              We have a unique network and skillset to help bring your projects
+              to life. Our small team of highly skilled individuals combined
+              with our large network put us in a strong position to deliver
+              exceptional results.
+            </p>
+            <p className="mb-6">
+              Over the past 10 years, we have worked on all kinds of projects.
+              From stations to high-rise buildings, we create spaces that
+              inspire and delight.
+            </p>
+            <p className="mb-6">
+              We work closely with our clients so that we understand the
+              intricacies of each project. This allows us to work in harmony the
+              surrounding area to create truly stunning projects that will stand
+              the test of time.
+            </p>
+          </div>
         </div>
       </section>
     </div>
