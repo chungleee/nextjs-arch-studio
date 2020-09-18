2f7eb9505685bfa1940a42a5f16e52121cfddf1d
Leon <leon.karho.chung@gmail.com>
2020-09-18 16:09:23 -0400

    styling

--- components/Button.js
+++ components/Button.js
@@ -4,7 +4,7 @@ import RightArrow from "./icons/RightArrow";
 const Button = ({ children, className }) => {
   return (
     <button
-      className={`flex bg-very-dark-blue items-center py-4 px-6 text-white hover:bg-dark-grey ${className}`}
+      className={`flex font-bold text-sm bg-very-dark-blue items-center py-4 px-6 text-white hover:bg-dark-grey ${className}`}
     >
       {children}{" "}
       <span className="ml-6">
