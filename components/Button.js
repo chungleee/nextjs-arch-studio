d3e1c298f3a4dac2dc586c4e99fc72e116261f38
Leon <leon.karho.chung@gmail.com>
2020-09-25 15:03:31 -0400

    center with justice

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
 import RightArrow from "./icons/RightArrow";
 
-const Button = ({ children, className }) => {
+const Button = ({ children, className, style }) => {
   return (
     <button
-      className={`flex font-bold text-sm bg-very-dark-blue items-center py-4 px-6 text-white hover:bg-dark-grey ${className}`}
+      className={`flex font-bold text-sm bg-very-dark-blue items-center justify-center py-4 px-6 text-white hover:bg-dark-grey ${className}`}
+      style={style}
     >
       {children}{" "}
       <span className="ml-6">
