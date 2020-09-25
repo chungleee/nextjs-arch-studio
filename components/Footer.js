a4108760718215721846a3816f64766e0d016017
Leon <leon.karho.chung@gmail.com>
2020-09-25 15:04:10 -0400

    tablet styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,32 +3,33 @@ import Button from "./Button";
 
 const Footer = () => {
   return (
-    <footer className="relative bg-very-light-grey" style={{ height: "445px" }}>
-      <div className="bg-white" style={{ height: "60px" }}></div>
-      <div
-        className="absolute bg-very-dark-blue flex justify-center items-center inset-0 mx-auto"
-        style={{
-          width: "120px",
-          height: "120px",
-        }}
-      >
-        <img
-          style={{ height: "23px", width: "58px" }}
-          src="/assets/logo-white.svg"
-          alt="Arch Logo"
-        />
+    <footer className="relative bg-very-light-grey h-445px md:flex md:h-32 md:bg-white">
+      <div className="bg-white md:hidden" style={{ height: "60px" }}></div>
+      <div className="hidden md:flex md:w-4/5 md:bg-very-light-grey">
+        <div
+          className="absolute bg-very-dark-blue flex justify-center items-center h-120px inset-0 mx-auto md:static md:ml-0 md:mr-16 md:h-auto"
+          style={{
+            width: "120px",
+          }}
+        >
+          <img
+            style={{ height: "23px", width: "58px" }}
+            src="/assets/logo-white.svg"
+            alt="Arch Logo"
+          />
+        </div>
+
+        <ul className="flex flex-col items-center justify-between h-150px mt-24 mb-8 font-bold text-sm text-medium-grey md:flex-row md:m-0 md:h-auto">
+          <li>Portfolio</li>
+          <li className="md:mx-16">About Us</li>
+          <li>Contact</li>
+        </ul>
+        <div className="hidden md:bg-white"></div>
       </div>
 
-      <ul
-        className="flex flex-col items-center justify-between mt-24 mb-8 font-bold text-sm text-medium-grey"
-        style={{ height: "150px" }}
-      >
-        <li>Portfolio</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
-
-      <Button className="mx-auto">See Our Portfolio</Button>
+      <Button className="mx-auto md:absolute md:h-16 md:right-0 md:top-0 md:bottom-0 md:my-auto">
+        See Our Portfolio
+      </Button>
     </footer>
   );
 };
