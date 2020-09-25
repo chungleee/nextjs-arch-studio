88c7c04d0a4c432aae23f57433bf43dae84df391
Leon <leon.karho.chung@gmail.com>
2020-09-25 15:03:09 -0400

    import header and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import "../styles/index.css";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <div className="md:px-20 md:m-auto">
-      <Component {...pageProps} />
+    <div>
+      <div className="md:px-20 md:m-auto">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
