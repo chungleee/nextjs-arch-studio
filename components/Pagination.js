2df48825a8dc68358c6ec947e4dc8660290e4f02
Leon <leon.karho.chung@gmail.com>
2020-09-28 15:54:15 -0400

    responsiveness + generate page buttons based on how many hero images in parent container

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,20 +1,30 @@
 import PageButton from "./PageButton";
+import { nanoid } from "nanoid";
 
 const Pagination = ({ heroes, handleSelectHero }) => {
   return (
-    <>
+    <div className="hidden lg:flex">
       {heroes.map((_, index) => {
+        let key = nanoid();
         return (
           <span
+            key={key}
             onClick={() => {
               handleSelectHero(index);
             }}
           >
-            <PageButton>{index + 1}</PageButton>
+            <PageButton
+              key={key}
+              onClick={() => {
+                handleSelectHero(index);
+              }}
+            >
+              {index + 1}
+            </PageButton>
           </span>
         );
       })}
-    </>
+    </div>
   );
 };
 
