fda41797d6f20aeaf8586efb06003da522ceb625
Leon <leon.karho.chung@gmail.com>
2020-09-30 14:58:01 -0400

    static data

---
+++ public/data.js
@@ -0,0 +1,104 @@
+// HOME PAGE DATA
+export const features = [
+  {
+    name: "Project Del Sol",
+    image: "img-del-sol",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "228B Tower",
+    image: "img-228b",
+    tablet: "img-228b-tablet",
+    desktop: "img-228b-desktop",
+  },
+  {
+    name: "Le Prototype",
+    image: "img-prototype",
+    tablet: "img-prototype-tablet",
+    desktop: "img-prototype-desktop",
+  },
+];
+
+export const heroes = [
+  "img-paramour-desktop",
+  "img-federal-desktop",
+  "img-seraph-desktop",
+  "img-trinity-desktop",
+];
+
+// PORTFOLIO PAGE DATA
+export const products = [
+  {
+    name: "Project Del Sol",
+    mobile: "image-228b-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-del-sol-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-edelweiss-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-eebox-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-federal-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-hypers-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-netcry-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-paramour-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-prototype-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-seraph-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-sxiv-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+  {
+    name: "Project Del Sol",
+    mobile: "image-trinity-mobile",
+    tablet: "img-del-sol-tablet",
+    desktop: "img-del-sol-desktop",
+  },
+];
