29ec57cd8a5624fde4493f774d14caabda4fa3c5
Leon <leon.karho.chung@gmail.com>
2020-09-30 15:55:45 -0400

    dropdown menu for mobile + handler to toggle dropdown

--- components/Header.js
+++ components/Header.js
@@ -3,29 +3,44 @@ import Hamburger from "./icons/Hamburger";
 import Link from "next/link";
 
 const Header = () => {
+  const handleNavbar = () => {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+  };
+
   return (
-    <header className="border-none flex justify-between items-center px-8 py-8 md:px-0">
-      <Link href="/">
-        <img
-          className="h-8 cursor-pointer"
-          src="/assets/logo.svg"
-          alt="Arch Logo"
-        />
-      </Link>
-      <ul className="hidden font-bold text-medium-grey md:flex md:mx-auto lg:ml-24">
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li className="mx-16">
-          <Link href="/about-us">About Us</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-      <span className="md:hidden">
-        <Hamburger />
-      </span>
+    <header className="relative">
+      <div className="border-none flex justify-between items-center px-8 py-8 md:px-0">
+        <Link href="/">
+          <img
+            className="h-8 cursor-pointer"
+            src="/assets/logo.svg"
+            alt="Arch Logo"
+          />
+        </Link>
+        <ul
+          id="menu"
+          className="hidden absolute flex flex-col justify-evenly items-center left-0 h-240px w-full bg-very-light-grey font-bold text-black md:static md:bg-white md:flex md:flex-row md:h-auto md:text-medium-grey md:mx-auto lg:ml-24"
+          style={{ top: "5rem" }}
+        >
+          <li>
+            <Link href="#">Portfolio</Link>
+          </li>
+          <li className="md:mx-16">
+            <Link href="#">About Us</Link>
+          </li>
+          <li>
+            <Link href="#">Contact</Link>
+          </li>
+        </ul>
+        <span
+          id="burger"
+          onClick={handleNavbar}
+          className="md:hidden cursor-pointer"
+        >
+          <Hamburger />
+        </span>
+      </div>
     </header>
   );
 };
