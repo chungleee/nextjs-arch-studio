80c78ce47d6b37c46c61b43981dc2c7ed8578cab
Leon <leon.karho.chung@gmail.com>
2020-11-20 18:43:27 -0500

    about us mobile page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React from 'react';
+import LinkedInIcon from '../components/icons/LinkedInIcon';
+import TwitterIcon from '../components/icons/TwitterIcon';
+import LeaderCard from '../components/LeaderCard';
 
 const AboutUs = () => {
-  return <div>this is about us page</div>;
+	const leaderData = [
+		{
+			name: 'Jake Richard',
+			src: '/assets/about/desktop/avatar-jake.jpg',
+		},
+		{
+			name: 'Thompson Smith',
+			src: '/assets/about/desktop/avatar-thompson.jpg',
+		},
+		{
+			name: 'Jackson Rourke',
+			src: '/assets/about/desktop/avatar-jackson.jpg',
+		},
+		{
+			name: 'Maria Simpson',
+			src: '/assets/about/desktop/avatar-maria.jpg',
+		},
+	];
+	return (
+		<div>
+			<section>
+				<img
+					src='/assets/about/mobile/image-hero.jpg'
+					alt='Picture of laptop with architectural blueprints'
+				/>
+				<div className='bg-white py-20 px-6'>
+					<h1
+						className='font-bold mb-4 leading-3rem text-5xl text-very-dark-blue'
+						style={{ letterSpacing: '-1px' }}
+					>
+						Your team of professionals
+					</h1>
+					<p className='leading-6 tracking-normal text-dark-grey'>
+						Our team of world-class <br /> professionals will work with you
+						every step of the way. Strong relationships are at the core of
+						everything we do. This extends to the relationship our projects have
+						with their surroundings.
+					</p>
+				</div>
+			</section>
+
+			<hr className='ml-6 mb-20 w-16' color='#C8CCD8' />
+
+			<section className='px-6 pb-20'>
+				<h1 className='font-bold mb-4 leading-3rem text-5xl text-very-dark-blue capitalize'>
+					Our <br /> heritage
+				</h1>
+				<div className='leading-6 tracking-normal text-dark-grey'>
+					<p>
+						Founded in 2007, we started as a trio of architects. Our
+						complmentary skills and relentless attention to detail turned Arch
+						into one of the most sought after boutique firms in the country.
+					</p>
+					<p className='my-8'>
+						Specializing in Urban Design allowed us to focus on creating
+						exceptional structures that live in harmony with their surroundings.
+						We consider every detail from every surrounding element to inform
+						our designs.
+					</p>
+					<p>
+						Our small team of world-class professionals provides input on every
+						project.
+					</p>
+				</div>
+			</section>
+
+			<section className='px-6'>
+				<h1 className='font-bold mb-4 leading-3rem text-5xl text-very-dark-blue capitalize'>
+					the <br /> leaders
+				</h1>
+				{leaderData.map(({ src, name }) => {
+					return <LeaderCard src={src} name={name} />;
+				})}
+			</section>
+		</div>
+	);
 };
 
 export default AboutUs;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,105 +1,109 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    fontFamily: {
-      sans: ["Spartan", "sans-serif"],
-    },
-    extend: {
-      height: {
-        "445px": "445px",
-        "560px": "560px",
-        "240px": "240px",
-        "120px": "120px",
-        "150px": "150px",
-      },
-      width: {
-        "120px": "120px",
-        "150px": "150px",
-        "350px": "350px",
-        "445px": "445px",
-      },
-      fontSize: {
-        medium: "7.5rem",
-        huge: "12.5rem",
-      },
-      backgroundImage: (theme) => {
-        return {
-          // MOBILE
-          "img-paramour": "url('/assets/home/mobile/image-hero-paramour.jpg')",
-          "img-small-team": "url('/assets/home/mobile/image-small-team.jpg')",
-          "img-del-sol": "url('/assets/portfolio/mobile/image-del-sol.jpg')",
-          "img-228b": "url('/assets/portfolio/mobile/image-228b.jpg')",
-          "img-prototype":
-            "url('/assets/portfolio/mobile/image-prototype.jpg')",
-          // TABLET
-          "img-paramour-tablet":
-            "url('/assets/home/tablet/image-hero-paramour.jpg')",
-          "img-small-team-tablet":
-            "url('/assets/home/tablet/image-small-team.jpg')",
-          "img-del-sol-tablet":
-            "url('/assets/portfolio/tablet/image-del-sol.jpg')",
-          "img-228b-tablet": "url('/assets/portfolio/tablet/image-228b.jpg')",
-          "img-prototype-tablet":
-            "url('/assets/portfolio/tablet/image-prototype.jpg')",
-          // DESKTOP
-          "img-paramour-desktop":
-            "url('/assets/home/desktop/image-hero-paramour.jpg')",
-          "img-federal-desktop":
-            "url('/assets/home/desktop/image-hero-federal.jpg')",
-          "img-seraph-desktop":
-            "url('/assets/home/desktop/image-hero-seraph.jpg')",
-          "img-trinity-desktop":
-            "url('/assets/home/desktop/image-hero-trinity.jpg')",
-          "img-small-team-desktop":
-            "url('/assets/home/desktop/image-small-team.jpg')",
-          "img-del-sol-desktop":
-            "url('/assets/portfolio/desktop/image-del-sol.jpg')",
-          "img-228b-desktop": "url('/assets/portfolio/desktop/image-228b.jpg')",
-          "img-prototype-desktop":
-            "url('/assets/portfolio/desktop/image-prototype.jpg')",
-          // PORTFOLIO
-          "image-228b-mobile":
-            'url("/assets/portfolio/mobile/image-228b-mobile.jpg")',
-          "image-del-sol-mobile":
-            'url("/assets/portfolio/mobile/image-del-sol-mobile.jpg")',
-          "image-edelweiss-mobile":
-            'url("/assets/portfolio/mobile/image-edelweiss-mobile.jpg")',
-          "image-eebox-mobile":
-            'url("/assets/portfolio/mobile/image-eebox-mobile.jpg")',
-          "image-federal-mobile":
-            'url("/assets/portfolio/mobile/image-federal-mobile.jpg")',
-          "image-hypers-mobile":
-            'url("/assets/portfolio/mobile/image-hypers-mobile.jpg")',
-          "image-netcry-mobile":
-            'url("/assets/portfolio/mobile/image-netcry-mobile.jpg")',
-          "image-paramour-mobile":
-            'url("/assets/portfolio/mobile/image-paramour-mobile.jpg")',
-          "image-prototype-mobile":
-            'url("/assets/portfolio/mobile/image-prototype-mobile.jpg")',
-          "image-seraph-mobile":
-            'url("/assets/portfolio/mobile/image-seraph-mobile.jpg")',
-          "image-sxiv-mobile":
-            'url("/assets/portfolio/mobile/image-sxiv-mobile.jpg")',
-          "image-trinity-mobile":
-            'url("/assets/portfolio/mobile/image-trinity-mobile.jpg")',
-        };
-      },
-      colors: {
-        "very-dark-blue": "#1B1D23",
-        "dark-grey": "#60636D",
-        "medium-grey": "#7D828F",
-        "light-grey": "#C8CCD8",
-        "very-light-grey": "#EEEFF4",
-        "error-red": "#DF5656",
-      },
-    },
-  },
-  variants: {
-    backgroundColor: ["responsive", "hover", "active"],
-  },
-  plugins: [],
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: [],
+	theme: {
+		fontFamily: {
+			sans: ['Spartan', 'sans-serif'],
+		},
+		extend: {
+			lineHeight: {
+				'3rem': '3rem',
+			},
+			height: {
+				'445px': '445px',
+				'560px': '560px',
+				'240px': '240px',
+				'120px': '120px',
+				'150px': '150px',
+			},
+			width: {
+				'120px': '120px',
+				'150px': '150px',
+				'345px': '345px',
+				'350px': '350px',
+				'445px': '445px',
+			},
+			fontSize: {
+				medium: '7.5rem',
+				huge: '12.5rem',
+			},
+			backgroundImage: (theme) => {
+				return {
+					// MOBILE
+					'img-paramour': "url('/assets/home/mobile/image-hero-paramour.jpg')",
+					'img-small-team': "url('/assets/home/mobile/image-small-team.jpg')",
+					'img-del-sol': "url('/assets/portfolio/mobile/image-del-sol.jpg')",
+					'img-228b': "url('/assets/portfolio/mobile/image-228b.jpg')",
+					'img-prototype':
+						"url('/assets/portfolio/mobile/image-prototype.jpg')",
+					// TABLET
+					'img-paramour-tablet':
+						"url('/assets/home/tablet/image-hero-paramour.jpg')",
+					'img-small-team-tablet':
+						"url('/assets/home/tablet/image-small-team.jpg')",
+					'img-del-sol-tablet':
+						"url('/assets/portfolio/tablet/image-del-sol.jpg')",
+					'img-228b-tablet': "url('/assets/portfolio/tablet/image-228b.jpg')",
+					'img-prototype-tablet':
+						"url('/assets/portfolio/tablet/image-prototype.jpg')",
+					// DESKTOP
+					'img-paramour-desktop':
+						"url('/assets/home/desktop/image-hero-paramour.jpg')",
+					'img-federal-desktop':
+						"url('/assets/home/desktop/image-hero-federal.jpg')",
+					'img-seraph-desktop':
+						"url('/assets/home/desktop/image-hero-seraph.jpg')",
+					'img-trinity-desktop':
+						"url('/assets/home/desktop/image-hero-trinity.jpg')",
+					'img-small-team-desktop':
+						"url('/assets/home/desktop/image-small-team.jpg')",
+					'img-del-sol-desktop':
+						"url('/assets/portfolio/desktop/image-del-sol.jpg')",
+					'img-228b-desktop': "url('/assets/portfolio/desktop/image-228b.jpg')",
+					'img-prototype-desktop':
+						"url('/assets/portfolio/desktop/image-prototype.jpg')",
+					// PORTFOLIO
+					'image-228b-mobile':
+						'url("/assets/portfolio/mobile/image-228b-mobile.jpg")',
+					'image-del-sol-mobile':
+						'url("/assets/portfolio/mobile/image-del-sol-mobile.jpg")',
+					'image-edelweiss-mobile':
+						'url("/assets/portfolio/mobile/image-edelweiss-mobile.jpg")',
+					'image-eebox-mobile':
+						'url("/assets/portfolio/mobile/image-eebox-mobile.jpg")',
+					'image-federal-mobile':
+						'url("/assets/portfolio/mobile/image-federal-mobile.jpg")',
+					'image-hypers-mobile':
+						'url("/assets/portfolio/mobile/image-hypers-mobile.jpg")',
+					'image-netcry-mobile':
+						'url("/assets/portfolio/mobile/image-netcry-mobile.jpg")',
+					'image-paramour-mobile':
+						'url("/assets/portfolio/mobile/image-paramour-mobile.jpg")',
+					'image-prototype-mobile':
+						'url("/assets/portfolio/mobile/image-prototype-mobile.jpg")',
+					'image-seraph-mobile':
+						'url("/assets/portfolio/mobile/image-seraph-mobile.jpg")',
+					'image-sxiv-mobile':
+						'url("/assets/portfolio/mobile/image-sxiv-mobile.jpg")',
+					'image-trinity-mobile':
+						'url("/assets/portfolio/mobile/image-trinity-mobile.jpg")',
+				};
+			},
+			colors: {
+				'very-dark-blue': '#1B1D23',
+				'dark-grey': '#60636D',
+				'medium-grey': '#7D828F',
+				'light-grey': '#C8CCD8',
+				'very-light-grey': '#EEEFF4',
+				'error-red': '#DF5656',
+			},
+		},
+	},
+	variants: {
+		backgroundColor: ['responsive', 'hover', 'active'],
+	},
+	plugins: [],
 };
