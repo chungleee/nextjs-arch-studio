3b668d4012d73adc5afcca4845993a24dc981578
chungleee <leon.karho.chung@gmail.com>
2021-01-22 11:44:16 -0500

    portfolio page json data

---
+++ data/portfolio.js
@@ -0,0 +1,122 @@
+export const data = [
+	{
+		title: 'seraph station',
+		month: 'september',
+		year: 2019,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-seraph.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-seraph.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-seraph.jpg',
+		},
+	},
+	{
+		title: 'eebox building',
+		month: 'august',
+		year: 2017,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-eebox.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-eebox.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-eebox.jpg',
+		},
+	},
+	{
+		title: 'federal II tower',
+		month: 'march',
+		year: 2017,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-federal.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-federal.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-federal.jpg',
+		},
+	},
+	{
+		title: 'project del sol',
+		month: 'january',
+		year: 2016,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-del-sol.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-del-sol.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-del-sol.jpg',
+		},
+	},
+	{
+		title: 'le prototype',
+		month: 'october',
+		year: 2015,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-prototype.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-prototype.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-prototype.jpg',
+		},
+	},
+	{
+		title: '228B tower',
+		month: 'april',
+		year: 2015,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-228b.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-228b.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-228b.jpg',
+		},
+	},
+	{
+		title: 'grand edelweiss hotel',
+		month: 'december',
+		year: 2013,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-edelweiss.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-edelweiss.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-edelweiss.jpg',
+		},
+	},
+	{
+		title: 'netcry tower',
+		month: 'august',
+		year: 2012,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-netcry.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-netcry.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-netcry.jpg',
+		},
+	},
+	{
+		title: 'hypers',
+		month: 'january',
+		year: 2012,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-hypers.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-hypers.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-hypers.jpg',
+		},
+	},
+	{
+		title: 'SXIV tower',
+		month: 'march',
+		year: 2011,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-sxiv.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-sxiv.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-sxiv.jpg',
+		},
+	},
+	{
+		title: 'trinity bank tower',
+		month: 'september',
+		year: 2010,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-trinity.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-trinity.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-trinity.jpg',
+		},
+	},
+	{
+		title: 'project paramour',
+		month: 'february',
+		year: 2008,
+		images: {
+			mobile: 'public/assets/portfolio/mobile/image-paramour.jpg',
+			tablet: 'public/assets/portfolio/tablet/image-paramour.jpg',
+			desktop: 'public/assets/portfolio/desktop/image-paramour.jpg',
+		},
+	},
+];
