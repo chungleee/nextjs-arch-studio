cad445e88bf656365cc0d51f04436a9013ed1e63
chungleee <leon.karho.chung@gmail.com>
2021-02-01 16:18:25 -0500

    map data into portfolio card component

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PortfolioCard from '../components/PortfolioCard';
+import { data } from '../data/portfolio';
 
 const Portfolio = () => {
-	return <div>this is portfolio page check responsively apppppp</div>;
+	console.log('portoflio data: ', data);
+	return (
+		<main>
+			{data.map((item) => {
+				return <PortfolioCard item={item} key={item.title} />;
+			})}
+		</main>
+	);
 };
 
 export default Portfolio;
