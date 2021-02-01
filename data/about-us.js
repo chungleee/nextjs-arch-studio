2f7270508e143586ad052619f293f0581771ffa4
chungleee <leon.karho.chung@gmail.com>
2021-02-01 16:17:09 -0500

    export leader data array

---
+++ data/about-us.js
@@ -0,0 +1,18 @@
+export const leaderData = [
+	{
+		name: 'Jake Richard',
+		src: '/assets/about/desktop/avatar-jake.jpg',
+	},
+	{
+		name: 'Thompson Smith',
+		src: '/assets/about/desktop/avatar-thompson.jpg',
+	},
+	{
+		name: 'Jackson Rourke',
+		src: '/assets/about/desktop/avatar-jackson.jpg',
+	},
+	{
+		name: 'Maria Simpson',
+		src: '/assets/about/desktop/avatar-maria.jpg',
+	},
+];
