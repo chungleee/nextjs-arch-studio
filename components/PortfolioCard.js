9f0c3fd08fbaf1a14fad96b93aed3db7ec533a05
chungleee <leon.karho.chung@gmail.com>
2021-02-01 16:59:49 -0500

    use picture > source for responsive images

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -4,12 +4,19 @@ const PortfolioCard = ({ item }) => {
 	console.log(item);
 	return (
 		<div>
-			<img
-				src={item.images.mobile}
-				alt={item.title}
-				srcSet={`${item.images.desktop} 1280w,
-                ${item.images.tablet} 768w`}
-			/>
+			<picture>
+				<source
+					media='(min-width: 1280px)'
+					srcSet={`${item.images.desktop}`}
+					alt={`${item.title}`}
+				/>
+				<source
+					media='(min-width: 768px)'
+					srcSet={`${item.images.tablet}`}
+					alt={`${item.title}`}
+				/>
+				<img src={`${item.images.mobile}`} alt={`${item.title}`} />
+			</picture>
 		</div>
 	);
 };
