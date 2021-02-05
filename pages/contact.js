6e346ebce1b251066cf7a96b08e2935039572c06
chungleee <leon.karho.chung@gmail.com>
2021-02-05 15:27:08 -0500

    use H1 component

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactDetailCard from '../components/ContactDetailCard';
+import H1 from '../components/H1';
 import { contactDetails } from '../data/contact';
 
 const Contact = () => {
@@ -13,9 +14,9 @@ const Contact = () => {
 			</picture>
 			<main>
 				<section className='pt-20 pb-10 px-6'>
-					<h1 className='font-bold text-left leading-none mb-4 text-4xl tracking-tight md:text-6xl'>
+					<H1>
 						Tell us about <br /> your project
-					</h1>
+					</H1>
 					<p className='leading-6 mb-12 text-left text-sm lg:w-445px'>
 						We’d love to hear more about your <br /> project. Please, leave a
 						message <br />
@@ -25,9 +26,9 @@ const Contact = () => {
 					</p>
 				</section>
 				<section className='pt-10 pb-20 px-6'>
-					<h1 className='capitalize font-bold leading-none mb-4 text-4xl tracking-tight md:text-6xl'>
-						contact <br /> details
-					</h1>
+					<H1>
+						Contact <br /> Details
+					</H1>
 					{contactDetails.map((contactDetail) => {
 						return (
 							<ContactDetailCard
@@ -36,6 +37,12 @@ const Contact = () => {
 							/>
 						);
 					})}
+				</section>
+
+				<section>
+					<H1>
+						Connect <br /> with us
+					</H1>
 				</section>
 			</main>
 		</div>
